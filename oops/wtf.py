ba3ccb1783b03c41f79b6c9c7ac6ce38300fdf1d
Bhavesh Vaviya <bhaveshvaviya48@gmail.com>
2023-06-14 22:20:42 +0530

    The ability to execute code directly within a class definition

---
+++ oops/wtf.py
@@ -0,0 +1,26 @@
+'''
+#^ What is the use of this kind of behavior ?
+'''
+
+def MatCallKaro():
+    print("Kyu kiya call")
+
+class WTF:
+    
+    #? The ability to execute code directly within a class definition -- Only "Python" has this feature
+    
+    print("kuch bhi chal rha he")
+    
+    if "*":
+        print("yeh bhi print ho gaya ???")
+    
+    try:
+        print("chale ?")
+    except Exception as e:
+        pass
+    
+    MatCallKaro()
+    
+    for i in range(1):
+        print("For statement")
+    
